8d4bbf41934f8ae4d9f85efa0e4a8ed0a7686295
Eugene Krivinsky <silver313@mail.ru>
2023-09-12 19:08:25 +0300

    Add project files.

---
+++ PowerUp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Task 5.5.8");
+
+Console.WriteLine(PowerUp(2, 2));
+Console.WriteLine(PowerUp(2, 3));
+Console.WriteLine(PowerUp(2, 4));
+
+static int PowerUp(int N, byte pow)
+{
+    if (pow == 0) return 1;
+    else
+    {
+    N = N * (PowerUp(N, --pow));
+
+    }
+    return N;
+}
