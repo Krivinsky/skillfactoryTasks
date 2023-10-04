f2b4f9814d5804a6b5cd757246f39ead5bc25e18
Eugene Krivinsky <silver313@mail.ru>
2023-10-04 18:02:55 +0300

    начало выполнения задания

---
+++ Class_and_struct/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var department = GetCurrentDepartment();
+        if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+            Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Type ?? "Неизвестная компания");
+        Console.WriteLine("У банка" + department.Company + " есть отделение в Санкт-Петербурге");
+    }
+
+    static Department GetCurrentDepartment()
+    {
+        return new Department();
+    }
+}
+
+
+class Company
+{
+    public string Type;
+    public string Name;
+}
+
+class Department
+{
+    public Company Company;
+    public City City;
+}
+
+class City
+{
+
+    public string Name;
+}
+
+
+class Bus
+{
+    public int? Load;
+
+    public void PrintStatus()
+    {
+        Console.WriteLine(Load ?? );
+    }
+}
---
+++ OnlineStore/Program.cs
@@ -0,0 +1,57 @@
+﻿abstract class Delivery
+{
+    public string Address;
+}
+
+class HomeDelivery : Delivery
+{
+    /* ... */
+}
+
+class PickPointDelivery : Delivery
+{
+    /* ... */
+}
+
+class ShopDelivery : Delivery
+{
+    /* ... */
+}
+
+class Order<TDelivery, TStruct> where TDelivery : Delivery
+{
+    public TDelivery Delivery;
+
+    public int Number;
+
+    public string Description;
+
+    public Product product;
+
+    public double weight;
+
+    public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+
+    // ... Другие поля
+}
+
+class Product<T> where T : ProductCategory  //описания товара
+{
+    T productCategory;
+    
+    //изготовитель
+}
+
+abstract class ProductCategory
+{
+
+}
+
+class Сustomer  // покупатель
+{
+
+}
+
