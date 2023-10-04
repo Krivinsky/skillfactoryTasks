8ab2d4676de9de57e0af2dcacd8ace494f6076d1
Eugene Krivinsky <Playa313@yandex.ru>
2023-10-04 21:50:18 +0300

    на проверку

---
+++ OnlineStore/Categories.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore
+{
+    public abstract class ProductCategory
+    {
+        int ID { get; set; }
+
+    }
+
+    public class Clothing : ProductCategory
+    {
+
+    }
+
+    public class Shoes : ProductCategory
+    {
+
+    }
+
+    public class Accessories : ProductCategory
+    {
+
+    }
+}
--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -1,57 +1,68 @@
-﻿abstract class Delivery
+﻿namespace OnlineStore
 {
-    public string Address;
-}
-
-class HomeDelivery : Delivery
-{
-    /* ... */
-}
-
-class PickPointDelivery : Delivery
-{
-    /* ... */
-}
-
-class ShopDelivery : Delivery
-{
-    /* ... */
-}
-
-class Order<TDelivery, TStruct> where TDelivery : Delivery
-{
-    public TDelivery Delivery;
-
-    public int Number;
-
-    public string Description;
-
-    public Product product;
-
-    public double weight;
-
-    public void DisplayAddress()
+    abstract class Delivery
     {
-        Console.WriteLine(Delivery.Address);
+        public string Address;
     }
 
-    // ... Другие поля
+    class HomeDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class ShopDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class Order<TDelivery, TStruct> where TDelivery : Delivery
+    {
+        static long serialNomber;
+        
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public Product<ProductCategory> product;
+
+        public double weight;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+    }
+
+    class Product<T> where T : ProductCategory  //описания товара
+    {
+        public T productCategory;
+
+        public double Price { get; set; }
+
+        public string Name { get; set; }
+
+        //изготовитель
+    }
+
+    class Сustomer  // покупатель
+    {
+        private string name;
+        private string surename;
+        private string adress;
+
+        public Сustomer(string name,  string surename, string adress)
+        {
+            this.name = name;
+            this.surename = surename;
+            this.adress = adress;
+        }
+    }
+
 }
-
-class Product<T> where T : ProductCategory  //описания товара
-{
-    T productCategory;
-    
-    //изготовитель
-}
-
-abstract class ProductCategory
-{
-
-}
-
-class Сustomer  // покупатель
-{
-
-}
-
