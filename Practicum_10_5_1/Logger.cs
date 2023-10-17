e4dd752f670e6722bb8429361e8c1695f39bd63f
Eugene Krivinsky <Playa313@yandex.ru>
2023-10-17 22:39:07 +0300

    Выполнено задание 10.5.1

---
+++ Practicum_10_5_1/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_10_5_1
+{
+    public class Logger : ILogger
+    {
+        public void Error(string str)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(str);
+        }
+
+        public void Event(string str)
+        {
+            //Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(str);
+        }
+    }
+
+    interface ILogger
+    {
+        void Event(string str);
+        void Error(string str);
+    }
+
+}
