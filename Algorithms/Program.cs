7db5122c8ffaed8b5bdc48794658bb25f25bbcda
Eugene Krivinsky <silver313@mail.ru>
2023-11-02 16:40:05 +0300

    выполнено задание 12.1.5

---
+++ Algorithms/Program.cs
@@ -0,0 +1,47 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Diagnostics;
+using System.Text;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Testing>();
+        }
+    }
+
+    public class Testing
+    {
+        static int Iterations = 10000;
+        [Benchmark]
+        public string UseString()
+        {
+            string value = "";
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                value += i.ToString();
+                value += " ";
+            }
+
+            return value;
+        }
+
+        [Benchmark]
+        public string UseStringBuilder()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                builder.Append(i.ToString());
+                builder.Append(" ");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
