d43dd68c980de50484ec6c9e72526ea5d678930d
Eugene Krivinsky <silver313@mail.ru>
2023-11-18 14:44:18 +0300

    Пройден урок 15.1

---
+++ Example15/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Example15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueSymbols();
+
+            var softwareManufacturers = new List<string>()
+            {
+                "Microsoft", "Apple", "Oracle"
+            };
+
+            var hardwareManufacturers = new List<string>()
+            {
+               "Apple", "Samsung", "Intel"
+            };
+
+            var itCompanies = softwareManufacturers.Union(hardwareManufacturers);
+
+
+        }
+
+        //15.1.4  Напишите метод для поиска общих букв в двух словах.
+        public static IEnumerable<char> GetCommonLetters(string input1, string input2)
+        {
+            return input1.ToCharArray().Intersect(input2.ToCharArray());
+        }
+
+
+        //Задание 15.1.6
+        //Напишите программу, которая будет принимать на вход строку текста с консоли
+        //(конструкция Console.Readline()) и выводить в ответ список содержащихся
+        //в строке уникальных символов без пробелов и следующих знаков препинания: , . ; :  ? !.
+
+        public static void UniqueSymbols()
+        {
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+
+            string except = ",.;: ?!";
+
+            var result = input.ToCharArray().Distinct().Except(except);
+
+            foreach (char c in result)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
