6318b5ed0eb0d0008238eae11c49e73684b9e6a2
Eugene Krivinsky <silver313@mail.ru>
2024-01-09 17:31:00 +0300

    Модуль 17

---
+++ ObserverExample/IObservable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverExample
+{
+    /// <summary>
+    /// Интерфейс наблюдаемого объекта
+    /// </summary>
+    interface IObservable
+    {
+        // добавить наблюдателя
+        void RegisterObserver(IObserver o);
+
+        // удалить наблюдателя
+        void RemoveObserver(IObserver o);
+
+        // уведомление наблюдателей
+        void NotifyObservers();
+    }
+}
---
+++ Skillfactory.Task16-7-1/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Skillfactory.Task16_7_1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
